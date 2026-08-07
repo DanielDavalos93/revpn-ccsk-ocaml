28712aee16fcc5636f335399b4ce5c9c151163df
DanielDavalos93 <dadaniel.1593@gmail.com>
2026-08-07 16:44:00 -0300

    editing for right execution of dune build

--- revpn-ccsk/enriching.ml
+++ revpn-ccsk/enriching.ml
@@ -1,5 +1,5 @@
-(* open Net *)
-
+open Net
+open Lts
 (** [token] is the set of tokens defined inductively as:
   - [Tok_empty]: {m (\cdot, \llangle \rrangle, \cdot)}
   - [Tok (a, ls, i)]: if [ls] has [token] type, {m (a, \llangle w_1,\dots,w_n\rrangle, i)} with {m a} an action, {m w_1,\dots,w_n} are tokens and {m i\in\mathbb N} an identificator number.
@@ -15,7 +15,7 @@ let rec size (tk : token) : int =
       List.fold_left (max) 0 (List.map (size) tok) + 1
 
 
-(** ---- Example 2 ---- *)
+(** ---- Example ---- *)
 
 let w0 = Tok_empty                        (* size w0 -> 0 *)
 let w1 = Tok ("a", [Tok_empty], 1)        (* size w1 -> 1 *)
