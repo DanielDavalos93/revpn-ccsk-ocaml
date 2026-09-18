b6a0ff90e0515202e717a2e384348eef4f54e1f4
DanielDavalos93 <dadaniel.1593@gmail.com>
2026-09-18 01:48:14 -0300

    key net and key place

--- revpn-ccsk/enriching.ml
+++ revpn-ccsk/enriching.ml
@@ -2,9 +2,12 @@ open Net
 open Lts
 open Util
 
-(** {b Token}
+(** 
+   [token] is the type of tokens defined inductively as:
 
-   [token] is the set of tokens defined inductively as:
+     {m \cfrac{}{\mathsf{Tok\_empty : token}} \qquad\qquad 
+     \cfrac{a\in\mathsf{A} \qquad ls\mathsf{: [token]} \qquad i \in \mathbb N}{\mathsf{Tok}(a, ls, i)} }
+
   - [Tok_empty]: {m (\cdot, \langle\!\langle \rangle\!\rangle, \cdot)}
   - [Tok (a, ls, i)]: if [ls] has [token] type, {m (a, \langle\!\langle w_1,\dots,w_n\rangle\!\rangle, i)} with {m a} an action, {m w_1,\dots,w_n} are tokens and {m i\in\mathbb N} an identificator number.
   *)
@@ -54,7 +57,7 @@ let w0 = Tok_empty                        (* size w0 -> 0 *)
 (** {m w_1 = (a, (\cdot, \langle\!\langle \rangle\!\rangle, \cdot), 1)} *)
 let w1 = Tok ("a", [Tok_empty], 1)        (* size w1 -> 1 *)
 
-(** {m w_2 = (b, (\cdot, \langle\!\langle \rangle\!\rangle, \cdot), 1)} *)
+(** {m w_2 = (b, \langle\!\langle (\cdot, \langle\!\langle \rangle\!\rangle, \cdot)\rangle\!\rangle, 1)} *)
 let w2 = Tok ("b", [Tok_empty], 1)        (* size w2 -> 1 *)
 
 (** {m w_3 = (a, \langle\!\langle (\cdot, \langle\!\langle \rangle\!\rangle, \cdot), 
@@ -63,7 +66,7 @@ let w3 = Tok ("a", [Tok_empty;
           Tok ("b", [Tok_empty], 1)], 2)  (* size w3 -> 2 *)
 
 (** {m w_4 = (\tau, \langle\!\langle(a, \langle\!\langle (\cdot, \langle\!\langle \rangle\!\rangle, \cdot), 
-    (b, (\cdot, \langle\!\langle \rangle\!\rangle, \cdot), 1)\rangle\!\rangle, 2)\rangle\!\rangle, 3)} *)
+    (b, \langle\!\langle(\cdot, \langle\!\langle \rangle\!\rangle, \cdot)\rangle\!\rangle, 1)\rangle\!\rangle, 2)\rangle\!\rangle, 3)} *)
 let w4 = Tok ("tau", [w3], 3)  (* size w4 -> 2 *)
 
 (* --------------------- *)
@@ -72,15 +75,30 @@ let w4 = Tok ("tau", [w3], 3)  (* size w4 -> 2 *)
     the notions for semantics. [token_net] is the type for nets where the 
     markings have type [token].
  *)
-
 type token_net = {
   net : labelled_net;
-  marking : token;
+  key : token;
 }
 
-let make_token_net (net : labelled_net) (tok_marking : token) : token_net = {
-  net = net; marking = tok_marking;
+let make_token_net (net : labelled_net) (tok : token) : token_net = {
+  net = net; key = tok;
 }
+
+(** {b Immediate dependency.} 
+    Two transitions {m t_1, t_2} are in {i immediate dependency} if 
+    there is a place {m a \in P} such that 
+    {m (t_1, a) \in R \wedge (a, t_2) \in R}.
+*)
+let immediate_transitions (ln : labelled_net) : (transition_id * transition_id) list =
+  let plSet = get_place ln in
+  let trSet = get_transition ln in
+  let binProd = bin_prod trSet trSet in
+  List.filter (fun pair ->
+    (List.exists (fun x ->
+      List.mem (TP (fst pair, x)) ln.arcs && List.mem (PT (x, snd pair)) ln.arcs
+    ) plSet)
+  ) binProd
+
 
 (** [m_flat marking] converts a marking into a flat token whose children
     are one [Tok(p, [Tok_empty], i)] per place, numbered left to right.
@@ -101,7 +119,6 @@ let make_token_net (net : labelled_net) (tok_marking : token) : token_net = {
 (*         List.mapi (fun i p -> Tok (label_a, [Tok_empty], i)) ps in *)
 (*       let top_id = encode_string label_a in *)
 (*       Tok (label_a, children, top_id) *)
-
 let rec m_flat (net : labelled_net) (marking : marking) : token =
   let lbl pid =
     match List.find_opt (fun p -> p.p_id = pid) net.places with
@@ -119,28 +136,6 @@ let rec m_flat (net : labelled_net) (marking : marking) : token =
       let child = m_flat net ps in
       Tok (lbl p, [m_flat net [p]; child], size child + 1)
 
-(** {b Immediate dependency.} 
-    Two transitions {m t_1, t_2} are in {i immediate dependency} if 
-    there is a place {m a \in P} such that 
-    {m (t_1, a) \in R \wedge (a, t_2) \in R}.
-*)
-let immediate_transitions (ln : labelled_net) : (transition_id * transition_id) list =
-  let plSet = get_place ln in
-  let trSet = get_transition ln in
-  let binProd = bin_prod trSet trSet in
-  List.filter (fun pair ->
-    (List.exists (fun x ->
-      List.mem (TP (fst pair, x)) ln.arcs && List.mem (PT (x, snd pair)) ln.arcs
-    ) plSet)
-  ) binProd
-  
-(* let marking_tok (ln : labelled_net) (m : marking) : token list = *)
-(*   ln.places |> List.mapi (fun i p -> (i+1, p)) |> *)
-(*   List.filter_map (fun (i, p) -> *)
-(**)
-(*     if List.mem p.p_id m then Some (Tok (p.p_id, [Tok_empty], i)) *)
-(*     else None *)
-(*   ) *)
 
 (** [is_enabled_tk tn tid] checks if transition [tid] is enabled
     in the token net [tn].
@@ -150,7 +145,7 @@ let immediate_transitions (ln : labelled_net) : (transition_id * transition_id) 
     token currently sits in one of the input places. *)
 let is_enabled_tk (tn : token_net) (tid : transition_id) : bool =
   let inputs = input_arcs tn.net tid in
-  match tn.marking with
+  match tn.key with
   | Tok_empty -> false
   | Tok (a, t, _) -> 
       (* let lamb_t = (tn.net.label_map {t_id = tid; t_label = ""}).t_label in *)
@@ -178,7 +173,7 @@ let fire_tk (tn : token_net) (tid : transition_id) : token_net option =
   if not (is_enabled_tk tn tid) then None
   else
     let outputs = output_arcs tn.net tid in
-    let w       = tn.marking in
+    let w       = tn.key in
     let base_id = next_id w in
     let new_token =
       match outputs with
@@ -193,13 +188,13 @@ let fire_tk (tn : token_net) (tid : transition_id) : token_net option =
           (* the new token is tagged with the transition id *)
           Tok ((tn.net.label_map {t_id = tid; t_label = ""}).t_id, children, base_id)
     in
-    Some { tn with marking = new_token }
+    Some { tn with key = new_token }
 
 (** [unfire_tk tn tid] reverses the firing of [tid].
     If the current token is [Tok (tid, [w], _)], returns [w].
     Returns [None] if the current token was not produced by [tid]. *)
 let unfire_tk (tn : token_net) (tid : transition_id) : token_net option =
-  match tn.marking with
+  match tn.key with
   | Tok_empty -> None
   | Tok (label, children, _) ->
       let outputs = output_arcs tn.net tid in
@@ -207,7 +202,7 @@ let unfire_tk (tn : token_net) (tid : transition_id) : token_net option =
       else
         match children with
         | [w] ->
-            Some { tn with marking = w }
+            Some { tn with key = w }
         | _ ->
            let inputs = input_arcs tn.net tid in
             begin match List.find_opt
@@ -216,7 +211,7 @@ let unfire_tk (tn : token_net) (tid : transition_id) : token_net option =
                 | Tok_empty   -> false)
               children
             with
-            | Some w -> Some { tn with marking = w }
+            | Some w -> Some { tn with key = w }
             | None   -> None
             end
 
@@ -230,23 +225,56 @@ let rec firing_sequence_tk (tn : token_net) (ts : transition_id list) : token_ne
       | None     -> None
       | Some tn' -> firing_sequence_tk tn' ts
 
+type keyPairNet = {
+  net : labelled_net;
+  key : label list;
+  }
+
+
 (** {b Key Labelled Net}
 
   A net {m K = (N, S_k)} is called {b key labelled net} (or simply key net)
   if for every {m t \in T}, {m |t^{\bullet} \cap S_k| = 1}, and for all 
   {m s \in S_k}, such that {m |s^{\bullet}|=0 \wedge |{}^{\bullet} s|=1.}
 *)
-let key_net (tn : marked_net) : bool =
+let is_key_net (kn : keyPairNet) : bool =
     List.for_all (fun t ->
-      let post_t = output_arcs tn.net t in
-      List.length (intersect post_t tn.marking) = 1
-    ) (get_transition tn.net) &&
+      let post_t = output_arcs kn.net t in
+      List.length (intersect post_t kn.key) = 1
+    ) (get_transition kn.net) &&
     List.for_all (fun s ->
-      let pre_s = List.map (fun x -> x.t_id) (preset_of_place tn.net s) in
-      let post_s = List.map (fun x -> x.t_id) (postset_of_place tn.net s) in
+      let pre_s = List.map (fun x -> x.t_id) (preset_of_place kn.net s) in
+      let post_s = List.map (fun x -> x.t_id) (postset_of_place kn.net s) in
       List.length pre_s = 1 && List.length post_s = 0
-    ) tn.marking
+    ) kn.key
 
 (** Example
  Using [net1] from [Net.ml] we define a token net with marking *)
-(* let tn1 = *)
+let key3 : place list = List.map (fun i -> 
+    "st" ^ string_of_int i |> make_place) (1--4)
+
+let pl3 : place list = (generate_place 4) @ key3
+
+let arcs3 = [
+  PT ("s1", "t1"); TP ("t1", "s1");
+  TP ("t1", "st1"); 
+  PT ("s1", "t2"); TP ("t2", "st2");
+  TP ("t1", "s2"); TP ("t2", "s3");
+  PT ("s3", "t4"); PT ("s2", "t4");
+  PT ("s2", "t3"); TP ("t4", "st4");
+  TP ("t4", "s1"); TP ("t3", "st3");
+  TP ("t3", "s4"); TP ("t4", "s4")
+]
+
+let net3 = make_label_net pl3 tr arcs3 set label_trans
+
+let knet3 : keyPairNet = 
+  {net = net3; 
+  key = List.map (fun x -> x.p_id) key3}
+
+(** {b Key place} *)
+let keypl (kn : keyPairNet) (t : transition_id) =
+  let sk = kn.key in
+  List.filter (fun x -> 
+    (List.map (fun y -> y.t_id) (preset_of_place kn.net x)) = [t]) sk
+
